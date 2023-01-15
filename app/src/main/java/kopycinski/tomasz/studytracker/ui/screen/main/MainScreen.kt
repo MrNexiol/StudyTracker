9ffdb856589c036365df75388b7844f5e3b384d3
Tomaszowy <mrnexiol@gmail.com>
2023-01-15 13:01:47 +0100

    Added top app bar Added string resources Changed project configuration to not use wildcard imports

--- app/src/main/java/kopycinski/tomasz/studytracker/ui/screen/main/MainScreen.kt
+++ app/src/main/java/kopycinski/tomasz/studytracker/ui/screen/main/MainScreen.kt
@@ -3,14 +3,20 @@ package kopycinski.tomasz.studytracker.ui.screen.main
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.*
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
+import kopycinski.tomasz.studytracker.R
 
 @Composable
 fun MainScreen(
@@ -19,9 +25,17 @@ fun MainScreen(
     val studySessions by viewModel.studySessionList.collectAsState()
 
     Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.main_screen_title)) }
+            )
+        },
         floatingActionButton = {
             FloatingActionButton(onClick = { viewModel.save() }) {
-                Icon(imageVector = Icons.Filled.Add, contentDescription = "ADD")
+                Icon(
+                    imageVector = Icons.Filled.Add,
+                    contentDescription = stringResource(id = R.string.add_study_session)
+                )
             }
         }
     ) { paddingValues ->
@@ -33,5 +47,4 @@ fun MainScreen(
             }
         }
     }
-
 }
